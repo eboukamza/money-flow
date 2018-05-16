391909e3db862378198133ebdcb7ed0a17f8be2c
Elias Zain <ezain2t@gmail.com>
2018-05-16 21:06:14 +0200

    ramda: pipe, map and sum

--- money-flow.js
+++ money-flow.js
@@ -1,11 +1,11 @@
-const {compose} = require('ramda')
+const {compose, pipe, map, sum} = require('ramda');
 
 const rateAt = rate => amount => amount * rate
 const forInterval = (min, max) => amount => Math.max(Math.min(max || amount, amount) - min, 0)
 const taxBracket = (rateAt, forInterval) => compose(rateAt, forInterval)
 const taxCollect = (amount) => (sum, tax) => sum + tax(amount);
 const netAfter = tax => amount => amount - tax(amount)
-const taxApply = (amount, tax) => tax(amount)
+const taxApply = amount => tax => tax(amount)
 
 let bracket0 = taxBracket(rateAt(0), forInterval(0, 9807))
 let bracket14 = taxBracket(rateAt(0.14), forInterval(9807,  27086))
@@ -14,7 +14,7 @@ let bracket41 = taxBracket(rateAt(0.41), forInterval(72617, 153783))
 let bracket45 = taxBracket(rateAt(0.45), forInterval(153783))
 
 let irBrackets = [bracket0, bracket14, bracket30, bracket41, bracket45]
-const ir = (amount) => irBrackets.reduce(taxCollect (amount), 0)
+let ir = (amount) => irBrackets.reduce(taxCollect(amount), 0)
 
 const irDividendes = amount => ir(rateAt(0.6)(amount))
 const afterIrDividendes = netAfter(irDividendes)
@@ -23,7 +23,7 @@ const afterIrDividendes = netAfter(irDividendes)
 const isBracket15 = taxBracket(rateAt(0.15), forInterval(0, 38120))
 const isBracket28 = taxBracket(rateAt(0.28), forInterval(38120))
 const isBrackets = [isBracket15, isBracket28];
-const is = (amount) => isBrackets.reduce(taxCollect(amount), 0)
+const is = (amount) => compose(sum, map)(taxApply(amount), isBrackets)
 const afterIs = netAfter(is)
 
 // CRGCRDS
@@ -34,10 +34,11 @@ const afterCrgCrds = netAfter(crgCrds)
 const flatTax = rateAt(0.3)
 const afterFlatTax = netAfter(flatTax)
 
-const withoutFlatTax = [afterIs, afterCrgCrds, afterIrDividendes]
-const withFlatTax = [afterIs, afterFlatTax]
+let withoutFlatTax = pipe(afterIs, afterCrgCrds, afterIrDividendes)
+let withFlatTax = pipe(afterIs, afterFlatTax);
 
-const delta = (amount) => withoutFlatTax.reduce(taxApply, amount) - withFlatTax.reduce(taxApply, amount)
+// and then
+const delta = (amount) => withoutFlatTax(amount) - withFlatTax(amount)
 
 exports.delta = delta
 
