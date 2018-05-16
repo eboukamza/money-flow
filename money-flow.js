2026ab7960d7c6c527d3346d6f41b35aa8869a51
Elias Zain <ezain2t@gmail.com>
2018-05-16 20:40:54 +0200

    first tax: with tax collect

--- money-flow.js
+++ money-flow.js
@@ -1,6 +1,7 @@
 const rateAt = rate => amount => amount * rate
 const forInterval = (min, max) => amount => Math.max(Math.min(max || amount, amount) - min, 0)
 const taxBracket = (rate, interval) => amount => rate(interval(amount))
+const taxCollect = (amount) => (sum, tax) => sum + tax(amount);
 
 let bracket0 = taxBracket(rateAt(0), forInterval(0, 9807))
 let bracket14 = taxBracket(rateAt(0.14), forInterval(9807,  27086))
@@ -9,7 +10,7 @@ let bracket41 = taxBracket(rateAt(0.41), forInterval(72617, 153783))
 let bracket45 = taxBracket(rateAt(0.45), forInterval(153783))
 
 let irBrackets = [bracket0, bracket14, bracket30, bracket41, bracket45]
-const ir = (amount) => irBrackets.reduce((sum, tax) => sum + tax(amount), 0)
+const ir = (amount) => irBrackets.reduce(taxCollect (amount), 0)
 
 exports.ir = ir
 
