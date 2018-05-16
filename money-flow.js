294e102b420ff8e29acc7b87f709e8c9f31867c8
Elias Zain <ezain2t@gmail.com>
2018-05-16 20:40:54 +0200

    first tax: net amount

--- money-flow.js
+++ money-flow.js
@@ -2,6 +2,7 @@ const rateAt = rate => amount => amount * rate
 const forInterval = (min, max) => amount => Math.max(Math.min(max || amount, amount) - min, 0)
 const taxBracket = (rate, interval) => amount => rate(interval(amount))
 const taxCollect = (amount) => (sum, tax) => sum + tax(amount);
+const netAfter = tax => amount => amount - tax(amount)
 
 let bracket0 = taxBracket(rateAt(0), forInterval(0, 9807))
 let bracket14 = taxBracket(rateAt(0.14), forInterval(9807,  27086))
@@ -11,10 +12,12 @@ let bracket45 = taxBracket(rateAt(0.45), forInterval(153783))
 
 let irBrackets = [bracket0, bracket14, bracket30, bracket41, bracket45]
 const ir = (amount) => irBrackets.reduce(taxCollect (amount), 0)
+const netAfterIr = netAfter(ir)
 
 exports.ir = ir
+exports.netAfterIr = netAfterIr
 
-// try
-//const {ir} = require("./money-flow.js")
+// try it
+//const {ir, netAfter} = require("./money-flow.js")
 //ir(50000) // 9293.26
-
+//netAfterIr(50000) // 40706.74
